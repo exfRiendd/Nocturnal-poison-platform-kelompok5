25b963734fd2d445fc935b10ac51bdd56a2b412c
Fachri26 <fachriwiryansyah26@gmail.com>
2026-06-12 22:32:25 +0700

    feat: endpoint GET api/roads

--- php-traffic/app/Http/Controllers/TrafficController.php
+++ php-traffic/app/Http/Controllers/TrafficController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TrafficRoad;
 use Illuminate\Http\Request;
 
 class TrafficController extends Controller
 {
-    //
+    public function roads()
+    {
+        return response()->json(
+            TrafficRoad::all()
+        );
+    }
 }
