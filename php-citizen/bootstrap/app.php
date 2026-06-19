1ae7e897d1036a144b0f8c7b88e27fa190e6c1b1
Piere Valkyrie <pierevalkyrie@gmail.com>
2026-06-19 19:28:20 +0700

    feat(qa): add wearable simulator and citizen JWT middleware

--- php-citizen/bootstrap/app.php
+++ php-citizen/bootstrap/app.php
@@ -11,8 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+   ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'citizen.auth' => \App\Http\Middleware\ExtractCitizenFromToken::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
