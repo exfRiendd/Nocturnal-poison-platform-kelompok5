29c28bfe965d01cc78366e79697a551d04a7b8f2
Alysha Ananda Shafa <aizu796@gmail.com>
2026-06-23 20:23:34 +0700

    feat(iot): add ML prediction trigger in Node-RED after sensor saved; fix merge conflict bootstrap

--- php-environment/bootstrap/app.php
+++ php-environment/bootstrap/app.php
@@ -8,10 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-<<<<<<< Updated upstream
         apiPrefix: 'api',
-=======
->>>>>>> Stashed changes
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
