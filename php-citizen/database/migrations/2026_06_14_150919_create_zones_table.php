8aad7109a3cce45bcd9aed1fc44675f032d1d552
Fachri26 <fachriwiryansyah26@gmail.com>
2026-06-26 14:20:48 +0700

    fix: perbaikan konsistensi dari table zones di php-citizen

--- php-citizen/database/migrations/2026_06_14_150919_create_zones_table.php
+++ php-citizen/database/migrations/2026_06_14_150919_create_zones_table.php
@@ -4,7 +4,12 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up(): void {
+    public function up(): void
+    {
+        if (Schema::hasTable('zones')) {
+            return;
+        }
+
         Schema::create('zones', function (Blueprint $table) {
             $table->integer('id', true);
             $table->string('name', 100);
